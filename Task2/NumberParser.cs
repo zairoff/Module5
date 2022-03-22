b63a69a4d35bad4a087529a442fc9d4f67ce5046
Maruf <marufzoiroff@gmail.com>
2022-03-22 09:57:32 +0500

    implemented Task2, all tests passed

--- Task2/NumberParser.cs
+++ Task2/NumberParser.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Task2
 {
     public class NumberParser : INumberParser
     {
-        public int Parse(string stringValue)
+        public int Parse(string input)
         {
-            throw new NotImplementedException();
+            if (input == null)
+                throw new ArgumentNullException();
+
+            if (string.IsNullOrWhiteSpace(input))
+                throw new FormatException();
+
+            input = input.Replace(" ", "");
+            var len = input.Length;
+            var set = new HashSet<char>();
+            double number = 0;
+
+            for (int i = 0; i < len; i++)
+            {
+                if (FormatIsNotNumber(set, input[i], i))
+                    throw new FormatException();
+
+                set.Add(input[i]);
+
+                if (CheckCharacter(input[i]))
+                    continue;
+
+                number += Math.Pow(10, len - i - 1) * (int)char.GetNumericValue(input[i]);
+            }
+
+            number = input[0] == '-' ? -number : number;
+
+            if (number > int.MaxValue || number < int.MinValue)
+                throw new OverflowException();
+
+            return (int)number;
+        }
+
+        private static bool FormatIsNotNumber(HashSet<char> set, char ch, int index)
+        {
+            return ((set.Contains(ch) && CheckCharacter(ch))
+                    || (!char.IsDigit(ch) && index != 0)
+                    || (!char.IsDigit(ch) && !CheckCharacter(ch)));
+        }
+
+        private static bool CheckCharacter(char ch)
+        {
+            return ch == '+' || ch == '-';
         }
     }
 }
